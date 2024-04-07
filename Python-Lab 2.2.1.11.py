3487f0c4d51e651c2958a8b14246aee56f6405af
Katarzyna Chajdas <129684767+KatarzynaJustyna@users.noreply.github.com>
2024-04-07 07:48:42 +0200

    Add files via upload

---
+++ Python-Lab 2.2.1.11.py
@@ -0,0 +1,116 @@
+#Lab 2.1.1.6
+print("Wlazl kotek na plotek")
+print("Mam na imie Kasia")
+
+#Lab 2.1.1.18
+print("Kurs", "Programowania", "w", sep="***", end="...")
+print("Pythonie")
+
+#Lab 2.1.1.19
+print("    *    ")
+print("   * *   ")
+print("  *   *  ")
+print(" *     * ")
+print("***   ***")
+print("  *   *  ")
+print("  *   *  ")
+print("  *****  ")
+
+
+
+print("    *    ", "   * *   ", "  *   *  ", " *     * ", "***   ***", sep="\n")
+print("  *   *  ", "  *   *  ", "  *****  ", sep="\n")
+
+
+
+print("       *       ")
+print("      * *      ")
+print("     *   *     ")
+print("    *     *    ")
+print("   *       *   ")
+print("  *         *  ")
+print(" *           * ")
+print("****       ****")
+print("   *       *   ")
+print("   *       *   ")
+print("   *       *   ")
+print("   *********   ")
+
+
+
+print("    *    "*2)
+print("   * *   "*2)
+print("  *   *  "*2)
+print(" *     * "*2)
+print("***   ***"*2)
+print("  *   *  "*2)
+print("  *   *  "*2)
+print("  *****  "*2)
+
+
+#Lab 2.2.1.11
+print("\"Ucze sie\" \n\"\"jezyka\"\" \n\"\"\"Python\"\"\"")
+
+
+
+#Konwertowanie odleglosci
+distance = input("insert distance in kilometers:")
+
+millimeters = int(distance) * 1000000.0
+print("Distance in millimeters:", {millimeters})
+
+centimeters = int(distance) * 100000.0
+print("Distance in centimeters:", {centimeters})
+
+meters = int(distance) * 1000.0
+print("Distance in meters:", {meters})
+
+inches = int(distance) * 39370.07874016
+print("Distance in inches:", {inches})
+
+feets = int(distance) * 3280.839895013
+print("Distance in feets:", {feets})
+
+miles = int(distance) * 39370078.74016
+print("Distance in miles:", {miles})
+
+
+
+#Lab 2.4.1.7
+pomaranczowy_krol = 3
+agnieszka = 5
+adam = 6
+print("pomaranczowy_krol", "agnieszka", "adam", sep=",")
+pomaranczy_razem = (pomaranczowy_krol + agnieszka + adam)
+print("Calkowita liczba pomaranczy:", pomaranczy_razem)
+
+
+
+#Lab 2.4.1.9
+kilometers = 12.25
+mile = 7.38
+mile_na_kilometry = mile * 1.61
+kilometry_na_mile = kilometers * 0.621
+print(mile, "mil to", round(mile_na_kilometry, 2), "kilometrow")
+print(kilometers, "kilometrow to", round(kilometry_na_mile, 2), "mil")
+
+#kolejne zadanie
+usd = 36.54
+eur = 55.68
+dolary_na_euro = usd * 0.92
+euro_na_dolary = eur * 1.10
+print(usd, "dolarow to", round(dolary_na_euro, 3), "euro")
+print(eur, "euro to", round(euro_na_dolary, 3), "dolarow")
+
+
+
+
+
+
+
+
+
+
+
+
+      
